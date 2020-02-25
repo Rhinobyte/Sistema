89c2ae6959acc21f5e09ee5e5fb4b1a454af107a
Isaac Garcia <joelgarcia405@gmail.com>
2020-02-25 08:46:22 -0600

    Agregar archivos de proyecto.

---
+++ Sistema.Datos/Conexion.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Data.SqlClient
+Public Class Conexion
+
+    Public conn As SqlConnection
+
+
+    Public Sub New()
+        Me.conn = New SqlConnection("Data Source=DGI-SF02VMS\SQLEXPRESS;Initial Catalog=nomina;User ID=isaac;Password=Jigz2019")
+    End Sub
+
+End Class
---
+++ Sistema.Negocio/NCategoria.vb
@@ -0,0 +1,47 @@
+﻿Imports Sistema.Datos
+Imports Sistema.Entidades
+Public Class NCategoria
+    Public Function Listar() As DataTable
+        Try
+            Dim Datos As New DCategoria
+            Dim Tabla As New DataTable
+            Tabla = Datos.Listar()
+            Return Tabla
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function Buscar(valor As String) As DataTable
+        Try
+            Dim Datos As New DCategoria
+            Dim Tabla As New DataTable
+            Tabla = Datos.Buscar(valor)
+            Return Tabla
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Public Function Insertar(Obj As Categoria) As Boolean
+
+    End Function
+
+    Public Function Actualizar(Obj As Categoria) As Boolean
+
+    End Function
+
+    Public Function Eliminar(Id As Integer) As Boolean
+
+    End Function
+
+    Public Function Desactivar(Id As Integer) As Boolean
+
+    End Function
+
+    Public Function Activar(Id As Integer) As Boolean
+
+    End Function
+End Class
