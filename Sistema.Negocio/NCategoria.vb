25c2d6af8a470e61663c2c6743aea012fcabbbdf
Isaac Garcia <joelgarcia405@gmail.com>
2020-02-25 10:02:39 -0600

    Termina NCategoria

--- Sistema.Negocio/NCategoria.vb
+++ Sistema.Negocio/NCategoria.vb
@@ -26,22 +26,57 @@ Public Class NCategoria
     End Function
 
     Public Function Insertar(Obj As Categoria) As Boolean
-
+        Try
+            Dim Datos As New DCategoria
+            Datos.Insertar(Obj)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
     End Function
 
     Public Function Actualizar(Obj As Categoria) As Boolean
-
+        Try
+            Dim Datos As New DCategoria
+            Datos.Actualizar(Obj)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
     End Function
 
     Public Function Eliminar(Id As Integer) As Boolean
-
+        Try
+            Dim Datos As New DCategoria
+            Datos.Eliminar(Id)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
     End Function
 
     Public Function Desactivar(Id As Integer) As Boolean
-
+        Try
+            Dim Datos As New DCategoria
+            Datos.Desactivar(Id)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
     End Function
 
     Public Function Activar(Id As Integer) As Boolean
-
+        Try
+            Dim Datos As New DCategoria
+            Datos.Activar(Id)
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
     End Function
 End Class
